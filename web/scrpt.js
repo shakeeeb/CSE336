ff8e525e56345f5cd601d2ef65400402f199df3a
Shakeeb Saleh <Shakeeb8644@gmail.com>
2015-09-08 18:40:05 -0400

    another web application

---
+++ web/scrpt.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function countQuery(){
+    //counts whatever using jquery
+    var d = $('document').size(); // i hope this shit works
+    console.log(d);
+    var f = $('<div>')
+}
+
+
